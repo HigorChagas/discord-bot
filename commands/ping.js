e63c0ea35ecd75b8d1e7c870a9464c0353ae3518
HigorChagas <higornchagas@gmail.com>
2023-02-22 15:17:42 -0300

    Criando interação do bot com o usuário

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Ping')
-        .setDescription('Reponsde com Pong!'),
+        .setName('ping')
+        .setDescription('Responde com Pong!'),
 
     async execute(interaction) {
         await interaction.reply('Pong!');
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
 //dotenv
 const dotenv = require('dotenv');
@@ -9,7 +9,24 @@ const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 const fs = require('node:fs');
 const path = require('path');
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+for(const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    
+    if('data' in command && 'execute' in  command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`Esse comando em ${filePath} está com 'data' ou 'execute' ausente`);
+    }
+}
+
+console.log(client.commands);
 
 client.once(Events.ClientReady, c => {
 	console.log(`Pronto! Login realizado como ${c.user.tag}`);
